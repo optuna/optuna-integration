6a449c24c20fb603cc11287ec47de3fb9a22171c
DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>
2024-02-08 21:07:12 +0200

    added _lightgbm_tuner package.

---
+++ optuna_integration/_lightgbm_tuner/__init__.py
@@ -0,0 +1,18 @@
+from optuna_integration._lightgbm_tuner._train import train
+from optuna_integration._lightgbm_tuner.optimize import LightGBMTuner
+from optuna_integration._lightgbm_tuner.optimize import LightGBMTunerCV
+from optuna_integration._lightgbm_tuner.optimize import _imports
+
+if _imports.is_successful():
+    from optuna_integration._lightgbm_tuner.sklearn import LGBMClassifier
+    from optuna_integration._lightgbm_tuner.sklearn import LGBMModel
+    from optuna_integration._lightgbm_tuner.sklearn import LGBMRegressor
+
+__all__ = [
+    "LightGBMTuner",
+    "LightGBMTunerCV",
+    "LGBMClassifier",
+    "LGBMModel",
+    "LGBMRegressor",
+    "train",
+]
