3a98645a7411c66ed67d43ee92c029898c241674
Alex Haslam <alex.haslam@h2gopower.com>
2024-06-06 11:34:15 +0100

    Fix formatting

--- optuna_integration/botorch/__init__.py
+++ optuna_integration/botorch/__init__.py
@@ -3,11 +3,12 @@ from .botorch import ehvi_candidates_func
 from .botorch import logei_candidates_func
 from .botorch import qehvi_candidates_func
 from .botorch import qei_candidates_func
+from .botorch import qhvkg_candidates_func
+from .botorch import qkg_candidates_func
 from .botorch import qnehvi_candidates_func
 from .botorch import qnei_candidates_func
 from .botorch import qparego_candidates_func
-from .botorch import qkg_candidates_func
-from .botorch import qhvkg_candidates_func
+
 
 __all__ = [
     "BoTorchSampler",
@@ -19,5 +20,5 @@ __all__ = [
     "qnei_candidates_func",
     "qparego_candidates_func",
     "qkg_candidates_func",
-    "qhvkg_candidates_func"
+    "qhvkg_candidates_func",
 ]
