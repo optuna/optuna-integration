a00bc770cfa7dd2288f14367297da679ce4a17a1
yu9824 <58211916+yu9824@users.noreply.github.com>
2024-10-27 11:58:24 +0900

    refactor: rename params to kwargs

--- optuna_integration/sklearn/sklearn.py
+++ optuna_integration/sklearn/sklearn.py
@@ -622,7 +622,7 @@ class OptunaSearchCV(BaseEstimator):
         return self.study_.user_attrs
 
     def decision_function(
-        self, X: TwoDimArrayLikeType, **params: Any
+        self, X: TwoDimArrayLikeType, **kwargs: Any
     ) -> OneDimArrayLikeType | TwoDimArrayLikeType:
         """Call ``decision_function`` on the best estimator.
 
@@ -632,7 +632,7 @@ class OptunaSearchCV(BaseEstimator):
 
         self._check_is_fitted()
 
-        return self.best_estimator_.decision_function(X, **params)
+        return self.best_estimator_.decision_function(X, **kwargs)
 
     @property
     def inverse_transform(self) -> Callable[..., TwoDimArrayLikeType]:
@@ -647,7 +647,7 @@ class OptunaSearchCV(BaseEstimator):
         return self.best_estimator_.inverse_transform
 
     def predict(
-        self, X: TwoDimArrayLikeType, **params: Any
+        self, X: TwoDimArrayLikeType, **kwargs: Any
     ) -> OneDimArrayLikeType | TwoDimArrayLikeType:
         """Call ``predict`` on the best estimator.
 
@@ -657,9 +657,9 @@ class OptunaSearchCV(BaseEstimator):
 
         self._check_is_fitted()
 
-        return self.best_estimator_.predict(X, **params)
+        return self.best_estimator_.predict(X, **kwargs)
 
-    def predict_log_proba(self, X: TwoDimArrayLikeType, **params: Any) -> TwoDimArrayLikeType:
+    def predict_log_proba(self, X: TwoDimArrayLikeType, **kwargs: Any) -> TwoDimArrayLikeType:
         """Call ``predict_log_proba`` on the best estimator.
 
         This is available only if the underlying estimator supports
@@ -668,9 +668,9 @@ class OptunaSearchCV(BaseEstimator):
 
         self._check_is_fitted()
 
-        return self.best_estimator_.predict_log_proba(X, **params)
+        return self.best_estimator_.predict_log_proba(X, **kwargs)
 
-    def predict_proba(self, X: TwoDimArrayLikeType, **params: Any) -> TwoDimArrayLikeType:
+    def predict_proba(self, X: TwoDimArrayLikeType, **kwargs: Any) -> TwoDimArrayLikeType:
         """Call ``predict_proba`` on the best estimator.
 
         This is available only if the underlying estimator supports
@@ -679,7 +679,7 @@ class OptunaSearchCV(BaseEstimator):
 
         self._check_is_fitted()
 
-        return self.best_estimator_.predict_proba(X, **params)
+        return self.best_estimator_.predict_proba(X, **kwargs)
 
     @property
     def score_samples(self) -> Callable[..., OneDimArrayLikeType]:
