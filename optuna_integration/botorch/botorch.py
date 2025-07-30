3acc6527efea1aab926b48c0d9e6a4ac1ada0a4d
Luke Tsekouras <luke.tsekouras@gmail.com>
2025-07-30 07:57:28 +0000

    Be backwards compatible with older versions of botortch

--- optuna_integration/botorch/botorch.py
+++ optuna_integration/botorch/botorch.py
@@ -436,7 +436,12 @@ def qehvi_candidates_func(
 
     ref_point_list = ref_point.tolist()
 
-    acqf = monte_carlo.qLogExpectedHypervolumeImprovement(
+    if hasattr(monte_carlo, 'qLogExpectedHypervolumeImprovement'):
+        hypervol_improvement_method = monte_carlo.qLogExpectedHypervolumeImprovement
+    else:
+        hypervol_improvement_method = monte_carlo.qExpectedHypervolumeImprovement
+
+    acqf = hypervol_improvement_method(
         model=model,
         ref_point=ref_point_list,
         partitioning=partitioning,
