1f705340b3b8eae58f89035a33af6ff6326ec7c2
gen740 <54583542+gen740@users.noreply.github.com>
2026-05-27 17:18:19 +0900

    feat: Remove deprecated optuna-integration module

--- optuna_integration/__init__.py
+++ optuna_integration/__init__.py
@@ -6,11 +6,8 @@ from typing import TYPE_CHECKING
 
 
 _import_structure = {
-    "allennlp": ["AllenNLPExecutor", "AllenNLPPruningCallback"],
     "botorch": ["BoTorchSampler"],
     "catboost": ["CatBoostPruningCallback"],
-    "chainer": ["ChainerPruningExtension"],
-    "chainermn": ["ChainerMNStudy"],
     "cma": ["PyCmaSampler"],
     "comet": ["CometCallback"],
     "dask": ["DaskStorage"],
@@ -37,12 +34,8 @@ _import_structure = {
 
 __all__ = [
     "__version__",
-    "AllenNLPExecutor",
-    "AllenNLPPruningCallback",
     "BoTorchSampler",
     "CatBoostPruningCallback",
-    "ChainerMNStudy",
-    "ChainerPruningExtension",
     "CometCallback",
     "DaskStorage",
     "FastAIPruningCallback",
@@ -69,12 +62,8 @@ __all__ = [
 ]
 
 if TYPE_CHECKING:
-    from optuna_integration.allennlp import AllenNLPExecutor
-    from optuna_integration.allennlp import AllenNLPPruningCallback
     from optuna_integration.botorch import BoTorchSampler
     from optuna_integration.catboost import CatBoostPruningCallback
-    from optuna_integration.chainer import ChainerPruningExtension
-    from optuna_integration.chainermn import ChainerMNStudy
     from optuna_integration.cma import PyCmaSampler
     from optuna_integration.comet import CometCallback
     from optuna_integration.dask import DaskStorage
@@ -105,7 +94,7 @@ else:
 
         This class applies lazy import under `optuna_integration`, where submodules are imported
         when they are actually accessed. Otherwise, `import optuna` becomes much slower because it
-        imports all submodules and their dependencies (e.g., chainer, keras, lightgbm) all at once.
+        imports all submodules and their dependencies (e.g., keras, lightgbm) all at once.
         """
 
         __file__ = globals()["__file__"]
